2f99b22014632868569118a93a4321d6c3f3bd72
niteshdhankar18 <niteshdhankar06@gmail.com>
2025-07-20 15:45:23 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,22 +1,19 @@
 let string = "";
-let buttons = document.querySelectorAll('.button');
+let buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(e) {
     let value = e.target.innerHTML;
 
-    if (value === '=') {
-      try {
-        string = eval(string);
-      } catch (err) {
-        string = "Error";
-      }
-    } else if (value === 'C') {
+    if (value === "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (value === "C") {
       string = "";
+      document.querySelector("input").value = string;
     } else {
       string += value;
+      document.querySelector("input").value = string;
     }
-
-    document.querySelector('.input').value = string;
   });
 });
